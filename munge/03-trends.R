7321e026281aff879e9af13fefe599f76dfd3bd0
taliahagerty <thagerty@economicsandpeace.org>
2020-06-16 10:27:53 +1000

    everything I need

--- munge/03-trends.R
+++ munge/03-trends.R
@@ -4,7 +4,20 @@ trn <- trn[-c(1:3), -c(8:10)]
 
 trn <- trn %>%
   filter(`Offence type` == "Domestic violence related assault") %>%
-  gather(period, value, 3:7)
+  gather(period, value, 3:7) %>%
+ mutate(value = as.numeric(value)) %>%
+  rename(lga = `Local Government Area`) %>%
+  mutate(lga = gsub("Cootamundra-Gundagai", "Cootamundra-Gundagai Regional", lga)) %>%
+  mutate(lga = gsub("Greater Hume Shire", "Greater Hume", lga)) %>%
+  mutate(lga = gsub("Glen Innes Severn", "Glen Innes Severn Shire", lga)) %>%
+  mutate(lga = gsub("Upper Hunter Shire", "Upper Hunter", lga)) %>%
+  mutate(lga = gsub("Warrumbungle Shire", "Warrumbungle", lga))
+  
+  
+  
+  
+  
 
 latest <- trn %>%
-  filter(period == "Apr 2019 - Mar 2020")
+  filter(period == "Apr 2019 - Mar 2020") %>%
+  arrange(-value) 
--- src/map.R
+++ src/map.R
@@ -6,29 +6,60 @@
 library(rvest)
 library(rgdal)
 library(leaflet)
+library(raster)
 
 path <- "./data/NSW_LGA_POLYGON_shp.shx"
 
-library(raster)
 nsw <- shapefile(path)
 
-m1 <- match(tolower(nsw$NSW_LGA__3), tolower(latest$`Local Government Area`))
+key <- match(tolower(nsw$NSW_LGA__3), tolower(latest$lga))
 
-# # see which names to fix
-# m <- nsw$NSW_LGA__3[is.na(m1)]
+# # see which names to fix -- all matching now
+# m <- nsw$NSW_LGA__3[is.na(key)]
 # m <- m[!grepl("UNINCORPORATED", m)]
 
-nsw$value <- latest$value[m1]
+nsw$value <- latest$value[key]
+
+# # leaflet version
+# factpal <- colorNumeric("Spectral", nsw$value)
+# 
+# leaflet(nsw) %>% 
+#   addPolygons(fillColor = ~factpal(value), 
+#               label = paste(latest$lga[key], latest$value[key], sep = " : "),
+#               fillOpacity = 1, 
+#               weight = 1) %>% 
+#   addLegend(position = "bottomright", 
+#             pal = factpal, 
+#             values = ~value, 
+#             opacity = 1)
 
 
-factpal <- colorFactor("Spectral", nsw$value)
+# static version
 
-leaflet(nsw) %>% 
-  addPolygons(fillColor = ~factpal(value), 
-              label = paste(latest$lga[m1], latest$value[m1], sep = " : "),
-              fillOpacity = 1, 
-              weight = 1) %>% 
-  addLegend(position = "bottomright", 
-            pal = factpal, 
-            values = ~value, 
-            opacity = 1)
+#generate colour scale, for negative peace use
+nbins = 9
+# plotclrs = (-heat.colors(nbins))
+# plotclrs =  c( "#1A9850", "#91CF60", "#D9EF8B" ,"#FEE08B", "#FC8D59", "#D73027")
+
+plotclrs = c("#d73027", "#e04f3d", "#e86853", "#ef8069", "#f59681", "#faac9a", "#fdc1b3", "#ffd7cd")
+plotclrs = rev(plotclrs)
+
+# check distribution
+summary(latest$value)
+hist(latest$value, breaks="FD")
+
+# set the breaks manually
+# the first bin should have the top five most peaceful states, 
+# and the last bin should have the five least peaceful
+# try to distribute the other states in groups of seven or eight
+brks = c(1, 38.5, 71.75, 105, 172.55, 240.1, 296.0, 1002, max(latest$value)) 
+
+class <- classIntervals(latest$value[key], n = nbins, style="fixed", fixedBreaks=brks)
+
+colcode <- findColours(class, plotclrs) 
+
+plot(nsw, col=colcode, lwd = 0.05, family = "Effra")
+title("Domestic violence incidents by LGA, Apr 2019 - Mar 2020")
+legend(x = "bottomright", legend=names(attr(colcode, "table")),
+       fill=attr(colcode, "palette"), cex=0.8, bty="n",
+       title = "Number of incidents")

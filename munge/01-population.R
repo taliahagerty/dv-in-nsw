ed1bb0df595a51e8d4cfeb12ba14a0b57ac0dacd
taliahagerty <taliahagerty@gmail.com>
2023-09-05 13:41:12 +1000

    population projections all years

--- munge/01-population.R
+++ munge/01-population.R
@@ -27,12 +27,11 @@ pop <- pop[-c(1:3),-c(2:3)] %>%
          lga = gsub(" LGA", "", trimws(` Local Government Areas`))) %>%
   # note that "All LGAs" becomes "Alls"
   filter(age == "All ages",
-         year %in% c(2011:2021),
          complete.cases(.)) %>%
   distinct() %>%
   select(lga, year, value) %>%
   group_by(lga) %>%
-  complete(year = full_seq(2011:2021, 1)) %>%
+  complete(year = full_seq(min(year):max(year), 1)) %>%
   mutate(annual.pop = round(approx(year,value,year)$y)) %>%
   ungroup() %>%
   left_join(fix) %>%
